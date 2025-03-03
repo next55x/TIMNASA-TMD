ff56cae354fac943c7bb62630ed51fdeebd5a875
next55x <deploytech597@gmail.com>
2025-03-04 01:15:03 +0300

    Update misc.js

--- commandes/misc.js
+++ commandes/misc.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 const config = require('../config');
-const { cmd, commands } = require('../command');
+const { framework, zonkou } = require('../framework/zokou');
 const util = require("util");
 const { getAnti, setAnti, initializeAntiDeleteSettings } = require('../data/antidel');
 
